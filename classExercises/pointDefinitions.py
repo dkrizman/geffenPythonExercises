6dc4af0404643559502dc4e8b98bdf08e3da9159
dkrizman <d.krizman@live.com>
2023-02-17 14:25:57 +0200

    adding latest updates

---
+++ classExercises/pointDefinitions.py
@@ -0,0 +1,16 @@
+class point:
+    def __init__(self,x,y):
+        import math as m
+        self.move(x, y)
+        self.distance = m.sqrt(self.x**2 + self.y**2)
+    
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def reset(self):
+        self.move(0,0)
+        
+
+if __name__ == "__main__":
+    pass
